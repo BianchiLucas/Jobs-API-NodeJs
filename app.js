3db680cb732ab5f64f50c2c0aa7b255c8185424b
Lucas Bianchi <lucasbianchi92@gmail.com>
2022-08-02 20:45:21 -0300

    Security middlewares

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
-//connect DB
+// Security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('rate-limiter');
+
+// Connect DB
 const connectDB = require('./db/connect');
 
 // Authentication middleware 
@@ -17,8 +23,20 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+// Parser middleware
 app.use(express.json());
-// extra packages
+
+// Security middlewares
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,     //15 minutes
+    max: 100                      //limit each Ip to 100 requests per window
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use('/api/v1/auth', authRouter);
